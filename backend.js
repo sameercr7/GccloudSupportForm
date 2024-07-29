95f6c40909389a72f5cc21f543f557e814c7debc
sameercr7 <sameer05072001@gmail.com>
2024-07-29 23:42:34 +0530

    mine api

--- backend.js
+++ backend.js
@@ -1,1 +1,41 @@
-console.log(sameer)
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+app.post('/send-email', (req, res) => {
+    const { name, email, message, subject } = req.body;
+
+    const mailOptions = {
+        from: email,
+        to: 'sameer05072001@gmail.com',
+        subject: subject,
+        text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+        res.status(200).send('Email sent: ' + info.response);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
